18280a00e011a2f695ed4e0c8348ff6ea9930a49
Your Namebmagar <you@example.commagarbinod5@gmail.com>
2022-12-05 15:46:10 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="main-section">
+        <div className="col item-1">
+          <img src="/images/icon-sedans.svg" />
+          <h1>Sedans</h1>
+          <p>
+            Choose a sedan for its affordability and excellent fuel economy.
+            Ideal for cruising in the city or on your next road trip.
+          </p>
+          <button className="seadans-button">Learn More</button>
+        </div>
+
+        <div className="col item-2">
+          <img src="/images/icon-suvs.svg" />
+          <h1>SUVs</h1>
+          <p>
+            Take an SUV for its spacious interior, power, and versatility.
+            Perfect for your next family vacation and off-road adventures.
+          </p>
+          <button className="suv-button">Learn More</button>
+        </div>
+
+        <div className="col item-3">
+          <img src="/images/icon-luxury.svg" />
+          <h1>Luxury</h1>
+          <p>
+            Cruise in the best car brands without the bloated prices. Enjoy the
+            enhanced comfort of a luxury rental and arrive in style.
+          </p>
+          <button className="luxury">Learn More</button>
+        </div>
+      </section>
+    </>
   );
 }
 
